e8795f2425cfa877365bbcfb227be188cb3a1564
Alex Riley <alex.riley.ar12@gmail.com>
2021-10-25 12:26:55 -0500

    created table of powers with formatting.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -24,18 +24,42 @@ public class ControlFlowExercises {
 //        for(long x = 2; x <= 1000000; x*=x){
 //            System.out.println(x);
 //        }
-        int f = 1;
-        while(f <= 100){
-            if(f % 3==0 && f % 5 != 0){
-                System.out.println("Fizz");
-            }else if(f % 5 == 0 && f % 3 != 0){
-                System.out.println("Buzz");
-            }else if(f % 3 == 0 && f % 5 ==0){
-                System.out.println("FizzBuzz");
-            }else{
-                System.out.println(f);
+
+        //FIZZ BUZZ Question
+//        int f = 1;
+//        while(f <= 100){
+//            if(f % 3==0 && f % 5 != 0){
+//                System.out.println("Fizz");
+//            }else if(f % 5 == 0 && f % 3 != 0){
+//                System.out.println("Buzz");
+//            }else if(f % 3 == 0 && f % 5 ==0){
+//                System.out.println("FizzBuzz");
+//            }else{
+//                System.out.println(f);
+//            }
+//            f++;
+//        }
+
+
+        String continuing = null;
+        do {
+            System.out.println("What number would you like to go up to? ");
+            int userInput = scanner.nextInt();
+            if (userInput > 0) {
+                System.out.println("number | squared | cubed");
+                System.out.println("------ | ------ | ------ ");
+                for (int i = 1; i <= userInput; i++) {
+                    int number = i;
+                    int squared = i * i;
+                    int cubed = i * i * i;
+                    System.out.printf("%-7d|", number);
+                    System.out.printf("%-8d|", squared);
+                    System.out.printf("%d%n", cubed);
+                }
+                System.out.println("Do you want to continue? Y/N");
+                continuing = scanner.next();
             }
-            f++;
-        }
+
+        } while (continuing.equalsIgnoreCase("Y"));
     }
 }
