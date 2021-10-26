6efd0ee65038f2f043a03eb9aea3e53e77101c04
Alex Riley <alex.riley.ar12@gmail.com>
2021-10-26 10:52:15 -0500

    completed first portion of string exercise in java

---
+++ src/Hangman.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
---
+++ src/StringExercise.java
@@ -0,0 +1,21 @@
+public class StringExercise {
+    public static void main(String [] args){
+        String education = "We don't need no education";
+        System.out.println(education);
+        String newEducation = education.replace("education", "thought control");
+        System.out.println(newEducation);
+        System.out.println();
+
+        String look = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(look);
+        System.out.println();
+
+        String sentence = "In windows, the main drive is usually C:\\";
+        System.out.println(sentence);
+        System.out.println();
+
+        String backslashes = "I can do backslashes \\, double backslashes \\\\, \n and the amazing triple backslash \\\\\\!";
+        System.out.println(backslashes);
+
+    }
+}
