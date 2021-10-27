d4823c81ef274664c9e1f79f9b060949a4370413
Alex Riley <alex.riley.ar12@gmail.com>
2021-10-27 11:20:34 -0500

    completed java methods exercises

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,20 +1,25 @@
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.Scanner;
+
+import java.util.Random;
 
 public class MethodsExercises {
 
-//    public static void main(String [] args) {
+    public static void main(String [] args) {
+//        userInput();
+//        getInteger(1,10);
+//            factorialTree();
+        diceRoll();
+    }
+
+//    public static void userInput(){
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println("Give me two numbers to work with: ");
 //        int num1 = scanner.nextInt();
 //        int num2 = scanner.nextInt();
 //        System.out.println("Would you like to add, subtract, multiply, divide, or modulate?");
 //        String calculation = scanner.next();
-////        System.out.println(add(num1, num2));
+//        System.out.println(add(num1, num2));
 //        calculate(num1, num2, calculation);
-//
-//
 //    }
 //    public static void calculate(int num1, int num2, String calculation) {
 //        switch (calculation) {
@@ -58,14 +63,50 @@ public class MethodsExercises {
 //        return num1 % num2;
 //    }
 
-    public static int getInteger(int min, int max){
-        System.out.println(min);
-        System.out.println(max);
+//    public static int getInteger(int min, int max){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a number between 1 and 10: ");
+//        int userNum = scanner.nextInt();
+//        if(userNum < 1){
+//            System.out.println("Your number is too low. Let's go higher.");
+//            return getInteger(min, max);
+//        } else if(userNum > 10){
+//            System.out.println("Your number is too high. Let's go lower.");
+//            return getInteger(min, max);
+//        }else {
+//            return userNum;
+//        }
+//    }
+
+//    public static int factorialTree() {
+//        int fact = 1;
+//        int userInput = getInteger(1, 10);
+//        for(int i = 1; i <= userInput; i++) {
+//            fact = fact * i;
+//            System.out.println(i + "! = " + fact );
+//        }
+//            System.out.println("Factorial of " + userInput + " is: " + fact);
+//        return userInput;
+//    }
+
+    public static void diceRoll() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number between 1 and 10: ");
-        getInteger();
-        return scanner.nextInt();
+        Random rand = new Random();
+        boolean keepRolling = true;
+        do {
+            System.out.println("Would you like to roll your dice?");
+            if (scanner.next().equalsIgnoreCase("yes")) {
+                System.out.println("Enter the number of sides for your pair of dice");
+                int userNSides = scanner.nextInt();
+                int n = rand.nextInt(userNSides * 2);
+                System.out.println("You rolled " + n);
+            } else {
+                System.out.println("Maybe we can play another time.");
+                keepRolling = false;
+            }
+        } while(keepRolling);
     }
 
-}
+    }
+
 
